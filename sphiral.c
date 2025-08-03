7caba552b4dd0328ee782f7ecc72c97f6ebf085f
sanjana29207 <sanjana.a@s.amity.edu>
2025-08-03 20:27:51 +0530

    size has been changed

--- sphiral.c
+++ sphiral.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-#define size 11
+#define size 10
 
 int is_prime(int n) {
-    if (n < 2) return 0;
+    if (n < 3) return 0;
     
-for (int i = 2; i <= sqrt(n); i++)
+for (int i = 3; i <= sqrt(n); i++)
         if (n % i == 0) return 0;
     return 1;
 }
 int main() {
     char grid[size][size] = {0};
-    int x = size / 2, y = size / 2;
+    int x = size / 3, y = size / 3;
     int dx = 1, dy = 0, step = 1, val = 1;
 
     for (int s = 0; s < size * size; ) {
