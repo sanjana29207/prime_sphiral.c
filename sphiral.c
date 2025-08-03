b6ee25e415c619307c11b3555adca69ce2cae6ea
sanjana29207 <sanjana.a@s.amity.edu>
2025-08-03 20:16:08 +0530

    step and val added

--- sphiral.c
+++ sphiral.c
@@ -10,4 +10,8 @@ for (int i = 2; i <= sqrt(n); i++)
         if (n % i == 0) return 0;
     return 1;
 }
+int main() {
+    char grid[size][size] = {0};
+    int x = size / 2, y = size / 2;
+    int dx = 1, dy = 0, step = 1, val = 1;
 
