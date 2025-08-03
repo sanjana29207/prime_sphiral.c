778bce156492853d18ed61d9de5a217ba236361e
sanjana29207 <sanjana.a@s.amity.edu>
2025-08-03 20:37:56 +0530

    a and b is added

--- sphiral.c
+++ sphiral.c
@@ -12,17 +12,17 @@ for (int i = 3; i <= sqrt(n); i++)
 }
 int main() {
     char grid[size][size] = {0};
-    int x = size / 3, y = size / 3;
+    int a = size / 3, b = size / 3;
     int dx = 1, dy = 0, step = 1, val = 1;
 
     for (int s = 0; s < size * size; ) {
         for (int i = 0; i < step; i++, s++) {
             if (x >= 0 && x < size && y >= 0 && y < size)
-                grid[y][x] = is_prime(val++) ? '*' : '.';
-            x += dx; y += dy;
+                grid[b][a] = is_prime(val++) ? '*' : '.';
+            a += dx; b += dy;
 
 	     }
-        int temp = dx; dx = -dy; dy = temp;
+        int temp = da; da = -db; db = temp;
         if (dy == 0) step++;
     }
 for (int i = 0; i < size; i++) {
